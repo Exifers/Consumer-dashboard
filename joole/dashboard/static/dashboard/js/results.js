6a1c3b65e5d1a3fe4e3bfd95b8c5b2f774eb438f
Emmanuel Meric de Bellefon <meric.emmanuel@gmail.com>
2018-08-25 12:57:25 +0200

    [Job]: added cost display, chart, data analysis

--- joole/dashboard/static/dashboard/js/results.js
+++ joole/dashboard/static/dashboard/js/results.js
@@ -1,1 +1,75 @@
-// ----> YOUR CODE HERE <----
+window.onload = function () {
+  var ttcWatts = "<span style='\"'color: #6f75a8;'\"'>{label}: {y} Watts</span>";
+  var ttcEuros = "<span style='\"'color: #00bfd1;'\"'>{label}: {y}€</span>";
+  var conso_euro = from_django[0];
+  var conso_watt = from_django[1];
+  var chart = new CanvasJS.Chart("conso_graph", {
+    animationEnabled: true,
+    theme: "light2",
+    zoomEnabled: true,
+    animationDuration: 1080,
+    axisX:{
+      labelFontSize: 15,
+      interval: 1,
+      labelAngle: -50
+    },
+    axisY:{
+        title: "Watts",
+        titleFontColor: "#6f75a8",
+        labelFontColor: "#6f75a8",
+        gridThickness: 1,
+        titleFontSize: 24,
+        labelFontSize: 15,
+        gridThickness: 0,
+        tickThickness: 0
+    },
+    axisY2:{
+        title: "Euros",
+        titleFontColor: "#00bfd1",
+        labelFontColor: "#00bfd1",
+        gridThickness: 1,
+        titleFontSize: 24,
+        labelFontSize: 15,
+        gridThickness: 0,
+        tickThickness: 0,
+    },
+    data: [{
+      type: "line",
+      color: "#6f75a8",
+      dataPoints: [
+        { label: "Janvier",   y: conso_watt[0] , toolTipContent: ttcWatts },
+        { label: "Février",   y: conso_watt[1] , toolTipContent: ttcWatts },
+        { label: "Mars",      y: conso_watt[2] , toolTipContent: ttcWatts },
+        { label: "Avril",     y: conso_watt[3] , toolTipContent: ttcWatts },
+        { label: "Mai",       y: conso_watt[4] , toolTipContent: ttcWatts },
+        { label: "Juin",      y: conso_watt[5] , toolTipContent: ttcWatts },
+        { label: "Juillet",   y: conso_watt[6] , toolTipContent: ttcWatts },
+        { label: "Août",      y: conso_watt[7] , toolTipContent: ttcWatts },
+        { label: "Septembre", y: conso_watt[8] , toolTipContent: ttcWatts },
+        { label: "Octobre",   y: conso_watt[9] , toolTipContent: ttcWatts },
+        { label: "Novembre",  y: conso_watt[10], toolTipContent: ttcWatts },
+        { label: "Décembre",  y: conso_watt[11], toolTipContent: ttcWatts }
+      ]
+    },
+    {
+      type: "line",
+      color: "#00bfd1",
+      axisYType: "secondary",
+      dataPoints: [
+        { label: "Janvier",   y: conso_euro[0] , toolTipContent: ttcEuros },
+        { label: "Février",   y: conso_euro[1] , toolTipContent: ttcEuros },
+        { label: "Mars",      y: conso_euro[2] , toolTipContent: ttcEuros },
+        { label: "Avril",     y: conso_euro[3] , toolTipContent: ttcEuros },
+        { label: "Mai",       y: conso_euro[4] , toolTipContent: ttcEuros },
+        { label: "Juin",      y: conso_euro[5] , toolTipContent: ttcEuros },
+        { label: "Juillet",   y: conso_euro[6] , toolTipContent: ttcEuros },
+        { label: "Août",      y: conso_euro[7] , toolTipContent: ttcEuros },
+        { label: "Septembre", y: conso_euro[8] , toolTipContent: ttcEuros },
+        { label: "Octobre",   y: conso_euro[9] , toolTipContent: ttcEuros },
+        { label: "Novembre",  y: conso_euro[10], toolTipContent: ttcEuros },
+        { label: "Décembre",  y: conso_euro[11], toolTipContent: ttcEuros }
+      ]
+    }]
+  });
+  chart.render();
+}
--- joole/dashboard/views.py
+++ joole/dashboard/views.py
@@ -19,12 +19,66 @@ class ClientFormView(View):
 def results(request, client_id):
     conso_euro = []
     conso_watt = []
-    annual_costs = [0, 0]
-    is_elec_heating = True
-    dysfunction_detected = False
+    annual_costs = [None, None]
+    is_elec_heating = None
+    dysfunction_detected = None
 
     ###################################
-    # ----> YOUR CODE GOES HERE <---- #
+    elec_heating_ratio_threshold = 0.15
+    dysfunction_ratio_threshold = 0.15
+
+    def consoToList(conso):
+      if (conso == None):
+        return None
+      return list(map(lambda x : round(x, 2),\
+              [conso.janvier,
+              conso.fevrier,
+              conso.mars,
+              conso.avril,
+              conso.mai,
+              conso.juin,
+              conso.juillet,
+              conso.aout,
+              conso.septembre,
+              conso.octobre,
+              conso.novembre,
+              conso.decembre]))
+
+
+    conso_euro = consoToList(next(iter(Conso_eur.objects.all()
+          .filter(client_id=client_id)
+          .filter(year=2017)), None))
+
+    conso_watt = consoToList(next(iter(Conso_watt.objects.all()
+          .filter(client_id=client_id)
+          .filter(year=2017)), None))
+
+    if (conso_euro != None and conso_watt != None):
+
+      annual_conso_euro = sum(conso_euro)
+
+      annual_conso_watt = sum(conso_watt)
+
+      annual_costs = [
+        round(annual_conso_euro, 2),
+        round(sum(consoToList(next(iter(\
+          Conso_eur.objects.all()
+            .filter(client_id=client_id)
+            .filter(year=2016)),\
+          None))), 2)
+      ]
+
+      # analysis
+      average_watt = float(annual_conso_watt) / float(len(conso_watt))
+      winter_max_watt = max(conso_watt[10], conso_watt[11], conso_watt[0])
+      winter_ratio = float(winter_max_watt - average_watt) / float(average_watt)
+      is_elec_heating = winter_ratio > elec_heating_ratio_threshold
+
+      year_ratio = float(abs(annual_costs[0] - annual_costs[1])) /\
+                   float(annual_costs[1])
+      print(year_ratio)
+      dysfunction_detected = year_ratio > dysfunction_ratio_threshold
+
     ###################################
 
     context = {
